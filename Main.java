494e8065d50df53004ccaf1f2d5ed1d06c840f5b
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-14 12:32:28 +0000

    Now tells the user if all numbers are odd

--- Main.java
+++ Main.java
@@ -1,15 +1,28 @@
 /**
- *
- * @author 
+ * A method the tells the user if all the digits are odd or not
+ * @author Liana Bazzarella 
  */
 public class Main {
+
+  // tells the user if all the digits are odd or not
+  public static int allDigitsOdd(int integer){
+    while(integer > 10){
+      integer = integer / 10;
+      if(integer%2 == 0){
+        System.out.println("False");
+      } else{
+        System.out.println("True");
+      }
+    }
+    return integer;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // number used 
+    allDigitsOdd(13579);
   }
 }
